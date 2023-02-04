85a3a42acfd440a8eecf4451bb3d220dd40e4e9c
ArthurCamargo <arthurcamargo13@gmail.com>
2023-02-04 16:24:27 -0300

    Create a basic structure for the game, adding code for the ball and the paddle

---
+++ config.lua
@@ -0,0 +1,8 @@
+-- conf.lua
+-- Love config file
+
+love.conf = function (t)
+    t.console = true -- Enable the debug console 
+    t.window.width = 800 -- width in number of pixels
+    t.window.height = 600  -- height in numbers of pixels
+end
---
+++ main.lua
@@ -0,0 +1,12 @@
+ -- Author: Arthur Camargo
+-- Main file for the game
+
+local world = require('world')
+local entities = {
+    paddle = require('entities/paddle'),
+    ball = require('entities/ball')
+}
+
+
+print(entities.paddle.body)
+print(entities.ball)
