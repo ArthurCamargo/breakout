cb67193461a3cd5f6c88d04ac30bc13a75fb727a
ArthurCamargo <arthurcamargo13@gmail.com>
2023-02-04 16:29:37 -0300

    Remove the gravity and makes some changes to the world file

--- main.lua
+++ main.lua
@@ -7,6 +7,43 @@ local entities = {
     ball = require('entities/ball')
 }
 
+-- Boolean to keep track of whether our game is paused or not
+local paused = false
 
-print(entities.paddle.body)
-print(entities.ball)
+local key_map = {
+  escape = function()
+    love.event.quit()
+  end,
+  space = function()
+    paused = not paused
+  end
+}
+
+love.draw = function()
+  local ball_x, ball_y = entities.ball.body:getWorldCenter()
+  love.graphics.circle('fill', ball_x, ball_y, entities.ball.shape:getRadius())
+  love.graphics.polygon(
+    'line',
+    entities.paddle.body:getWorldPoints(entities.paddle.shape:getPoints())
+  )
+end
+
+love.focus = function(focused)
+  if not focused then
+    paused = true
+  end
+end
+
+love.keypressed = function(pressed_key)
+  -- Check in the key map if there is a function
+  -- that matches this pressed key's name
+  if key_map[pressed_key] then
+    key_map[pressed_key]()
+  end
+end
+
+love.update = function(dt)
+  if not paused then
+    world:update(dt)
+  end
+end
